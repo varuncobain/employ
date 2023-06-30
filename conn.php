236b26bb445f64be0d7e079766b0474d59c97c8c
Varun <chauhanvarun61@gmail.com>
2023-06-30 19:56:28 +0530

    Revert "seconf"
    This reverts commit 6687f701e4f6bb86fc89bc9d7ab8e954074cf302.

--- conn.php
+++ conn.php
@@ -1,6 +1,10 @@
 <?php
 
+<<<<<<< HEAD
 $conn=mysqli_connect('EC=XTRA BRANCH','root','password@123','test');
+=======
+$conn=mysqli_connect('localhosttt','root','password@123','test');
+>>>>>>> parent of 6687f70... seconf
 
 
 ?>
